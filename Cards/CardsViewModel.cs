58652a2a9c673db4553725f420ddb3191ad304e8
conpawel <pbaszuro@yahoo.pl>
2015-06-09 23:41:40 +0200

    Zadania

---
+++ Cards/CardsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Cards
+{
+    /// <summary>
+    /// Main class for View Model
+    /// TODO: follow guidelines
+    /// </summary>
+    public class CardsViewModel
+    {
+        private readonly IDispatcher _dispatcher;
+        public CardsViewModel(IDispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;
+        }
+    }
+}
